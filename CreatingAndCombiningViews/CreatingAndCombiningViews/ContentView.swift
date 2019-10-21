cca443a948eb1031eee8c4048ca082a571fb355c
makanai5610 <takumi.k.5610@gmail.com>
2019-10-21 20:59:43 +0900

    first commit

---
+++ CreatingAndCombiningViews/CreatingAndCombiningViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreatingAndCombiningViews
+//
+//  Created by Takumi Karibe on 2019/10/21.
+//  Copyright © 2019 Takumi Karibe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
